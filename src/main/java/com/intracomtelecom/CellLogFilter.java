13636c924d79e743302ceae73483a0281ae73cb9
Panagiotis Dimopoulos <panosdim@gmail.com>
2021-05-11 23:12:45 +0300

    Enable CORS.

--- src/main/java/com/intracomtelecom/CellLogFilter.java
+++ src/main/java/com/intracomtelecom/CellLogFilter.java
@@ -49,6 +49,28 @@ public class CellLogFilter {
             response.type("application/json");
             response.header("Content-Encoding", "gzip");
         });
+
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
     }
 
     public static String dataToJson(Object data) {
